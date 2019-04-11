a13ba0d83581fe3c8146982f975167e761c80ff0
JayWaaking <35748420+JayWaaking@users.noreply.github.com>
2019-04-11 09:03:01 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -64,7 +64,9 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 		{
 			if(cmd.trim() == raid[i].trim())
 			{
-				
+				var path = requires('path');
+				var raidName = path.join('/', __dirname, '/', cmd, '.txt';
+				bot.sendMessage({to: channelID, message: 'found it:: ' + raidName});
 			}
 		}
      }
